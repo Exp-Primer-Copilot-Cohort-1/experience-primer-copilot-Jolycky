24bf9cec1896734b5e31d4db3966acb70aa6e08b
Jose Bagus Ramadhan <126927999+Jolycky@users.noreply.github.com>
2024-05-08 04:31:29 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,66 @@
+// Create web server
+var express = require('express');
+var app = express();
+app.listen(3000, function() {
+    console.log('Server is running on http://localhost:3000');
+});
+
+// Create comment
+app.post('/comment', function(req, res) {
+    var comment = req.body.comment;
+    res.send('Comment: ' + comment);
+});
+
+// Create comment
+app.post('/comment', function(req, res) {
+    var comment = req.body.comment;
+    res.send('Comment: ' + comment);
+});
+
+// Create comment
+app.post('/comment', function(req, res) {
+    var comment = req.body.comment;
+    res.send('Comment: ' + comment);
+});
+
+// Create comment
+app.post('/comment', function(req, res) {
+    var comment = req.body.comment;
+    res.send('Comment: ' + comment);
+});
+
+// Create comment
+app.post('/comment', function(req, res) {
+    var comment = req.body.comment;
+    res.send('Comment: ' + comment);
+});
+
+// Create comment
+app.post('/comment', function(req, res) {
+    var comment = req.body.comment;
+    res.send('Comment: ' + comment);
+});
+
+// Create comment
+app.post('/comment', function(req, res) {
+    var comment = req.body.comment;
+    res.send('Comment: ' + comment);
+});
+
+// Create comment
+app.post('/comment', function(req, res) {
+    var comment = req.body.comment;
+    res.send('Comment: ' + comment);
+});
+
+// Create comment
+app.post('/comment', function(req, res) {
+    var comment = req.body.comment;
+    res.send('Comment: ' + comment);
+});
+
+// Create comment
+app.post('/comment', function(req, res) {
+    var comment = req.body.comment;
+    res.send('Comment: ' + comment);
+});
